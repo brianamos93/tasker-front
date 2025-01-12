02bd0097b775e228018720f696d0893a16bb1228
Brian <brianizzo93@gmail.com>
2025-01-12 21:13:04 +0900

    Changed to backend auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,77 +1,34 @@
-"use client";
-import { login } from '@/app/utils/authconnections'
-import { setToken } from '../utils/apiconnections';
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import Notification from '@/app/components/Notification'
+import { redirect } from "next/navigation";
+import { getSession, login, logout } from '@/app/utils/loginlib'
 
-export default function Home() {
-  const { push } = useRouter();
-  const [errorMessage, setErrorMessage] = useState<null | string>(null)
-  const [username, setUsername] = useState('') 
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState<null | string>(null)
-
-  const handleLogin = async (e: { preventDefault: () => void; }) => {
-    e.preventDefault()
-
-    try {
-      const user = await login({username, password})
-      window.localStorage.setItem(
-        'loggedTaskappUser', JSON.stringify(user)
-      )
-      setToken(user.token)
-      setUser(user)
-      setUsername('')
-      setPassword('')
-      //push('/task')
-    } catch (exception) {
-      setErrorMessage('Wrong Credentials')
-      console.log(exception)
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
-    }
-  }
-
+export default async function Page() {
+  const session = await getSession()
   return (
-    <main>
-      <Notification message={errorMessage} />
-      <h1>Login</h1>
-
-      <form onSubmit={handleLogin} className="flex flex-col gap-4">
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={username}
-            onChange={({ target }) => setUsername(target.value)}
-            required
-            className="border rounded border-black"
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-            required
-            className="border rounded border-black"
-          />
-        </div>
-
-        <button
-          type="submit"
-          className="p-2 bg-orange-600 text-white w-fit rounded"
-        >
-          Submit
+    <section>
+      <form action={async (formData) => {
+        'use server'
+        await login(formData)
+        redirect('/')
+      }}
+      >
+        <input type="text" name="username" id="username" placeholder="username" />
+        <br />
+        <input type="password" name="password" placeholder="password" id="password" />
+        <br />
+        <button type="submit">
+        Login
         </button>
       </form>
-    </main>
-  );
+      <form action={async () => {
+        'use server'
+        await logout()
+        redirect('/')
+      }}
+      >
+        <button type="submit">Logout</button>
+      </form>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
+    </section>
+  )
+
 }

9bea6065b35ea408454bc235e19b2c5eaee957ee
Brian <brianizzo93@gmail.com>
2025-01-20 00:19:25 +0900

    Moved middleware to top layer.

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse, type NextRequest } from 'next/server'
+import { getSession } from './app/utils/loginlib'
+
+
+
+ 
+export async function middleware(request: NextRequest) {
+  const protectedRoutes = ['/task/create', '/task/edit/:path']
+  const path = request.nextUrl.pathname
+  const isProtectedRoute = protectedRoutes.includes(path)
+  const session = await getSession()
+
+  if(isProtectedRoute && !session?.task)
+  return NextResponse.redirect(new URL('/login', request.nextUrl))
+
+
+    return NextResponse.next()
+
+
+}
+ 
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'],
+}
